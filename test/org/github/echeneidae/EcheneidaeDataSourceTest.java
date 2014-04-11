a8cae353c9c2d0efdede6ecd37b224c114db4bce
finallygo <357857613@qq.com>
2014-04-11 15:38:20 +0800

    add mutil thread test

--- test/org/github/echeneidae/EcheneidaeDataSourceTest.java
+++ test/org/github/echeneidae/EcheneidaeDataSourceTest.java
@@ -1,8 +1,13 @@
 package org.github.echeneidae;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.MapListHandler;
@@ -14,12 +19,17 @@ import org.junit.Test;
  * 2014-4-11 下午1:58:42
  */
 public class EcheneidaeDataSourceTest {
-
-	@Test
-	public void testGetConnection() throws SQLException {
+	
+	private EcheneidaeDataSource getDataSource() {
 		String driverClassName = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://localhost/test?connectTimeout=1000&socketTimeout=1000";
 		EcheneidaeDataSource ds = new EcheneidaeDataSource(driverClassName, url, "root", "");
+		return ds;
+	}
+
+	@Test
+	public void testGetConnection() throws SQLException {
+		EcheneidaeDataSource ds = this.getDataSource();
 		
 		QueryRunner queryer = new QueryRunner(ds);
 		List<Map<String, Object>> result = queryer.query("select * from tb_users", new MapListHandler());
@@ -39,6 +49,33 @@ public class EcheneidaeDataSourceTest {
 		}
 		result = queryer.query("select * from tb_users", new MapListHandler());
 		Assert.assertNotNull(result);
+	}
+	
+	@Test
+	public void testMutilThread() throws InterruptedException, ExecutionException {
+		ExecutorService threadPool = Executors.newFixedThreadPool(3);
+		final EcheneidaeDataSource ds = this.getDataSource();
+		final QueryRunner queryer = new QueryRunner(ds);
+		
+		List<Future<?>> futures = new ArrayList<Future<?>>();
+		for (int i = 0; i < 100; i++) {
+			Future<?> f = threadPool.submit(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						queryer.query("select * from tb_users", new MapListHandler());
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			futures.add(f);
+		}
+		
+		for (Future<?> future : futures) {
+			future.get();
+		}
 	}
 	
 }
